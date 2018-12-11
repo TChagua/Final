1db07ef8d72938adcde74dd0df0f0511241edfbc
TChagua <tea.chagua@gmail.com>
2018-12-12 01:23:53 +0400

    mgoni aitvirteba

---
+++ react-test/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom';
+
+const Main = () => {
+    return (<div>
+<Link to="/register">Register</Link>
+
+    </div>
+        
+    )
+}
+
+export default Main
