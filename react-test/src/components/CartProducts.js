bf2a5ce8fd9862503a49728abd786bdf1afa9492
TChagua <tea.chagua@gmail.com>
2018-12-14 15:38:06 +0400

    cart-products styles improved

--- react-test/src/components/CartProducts.js
+++ react-test/src/components/CartProducts.js
@@ -6,14 +6,12 @@ const CartProducts = (props) => {
         <div className="cart">
             <h1 className="title">Checkout</h1>
             <h3 className="sub-title">My Shopping Cart (1)</h3>
-            <div className="cart-header">
                 <ul className="cart-headings">
                     <li>Item</li>
                     <li>Product Name</li>
                     <li>Unit Price</li>
                     <li>Delivery Details</li>
                 </ul>
-            </div>
             <CartProduct />
             <button className="purchase">Purchase</button>
         </div>
--- react-test/src/components/common/CartProduct.js
+++ react-test/src/components/common/CartProduct.js
@@ -3,19 +3,16 @@ import { Link} from 'react-router-dom';
 
 const CartProduct = (props) => {
     return(
-
-        <div className="cart-item">
-            <ul className="cart-item-list">
-                <li><Link to={"/products/"}><img src="https://n3.sdlcdn.com/imgs/e/v/o/SDL044502225_1-6ee47.jpg" alt="" className="cart-item-image"/></Link></li>
-                <li>Timex</li>
-                <li>$290</li>
-                <li>
-                    <i className="fas fa-times"></i>
-                    <p>Free</p>
-                    <p>Delivered in 2-3 business days</p>
-                    </li>
-            </ul>
-        </div>
+        <ul className="cart-item-list">
+            <li><Link to={"/products/"}><img src="https://n3.sdlcdn.com/imgs/e/v/o/SDL044502225_1-6ee47.jpg" alt="" className="cart-item-image"/></Link></li>
+            <li>Timex</li>
+            <li>$290</li>
+            <li>
+                <i className="fas fa-times"></i>
+                <p>Free</p>
+                <p>Delivered in 2-3 business days</p>
+            </li>
+        </ul>
     )
 }
 export default CartProduct
